ece990a7ba24934c72ae9716f484c68adfc7784d
Tarık Taha ZIMBA <74347523+tariktaha@users.noreply.github.com>
2025-04-10 22:08:00 +0300

    Stok Takip Sistemi
    BonusMvcStok: ASP.NET MVC5 ile Geliştirilmiş Dinamik Stok ve Ürün Yönetim Paneli

---
+++ BonusMvcStok/BonusMvcStok/Controllers/SatislarController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusMvcStok.Models.Entity;
+
+namespace BonusMvcStok.Controllers
+{
+    public class SatislarController : Controller
+    {
+        // GET: Satislar
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var satislar = db.tblsatislar.ToList();
+            return View(satislar);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            //Ürünler
+            List<SelectListItem> urun = (from x in db.tblurunler.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad,
+                                            Value = x.id.ToString()
+                                        }).ToList();
+            ViewBag.drop1 = urun; //Sayfalar arası veri taşımak için
+
+            //Personeller
+            List<SelectListItem> per = (from x in db.tblpersonel.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad +" "+ x.soyad,
+                                            Value = x.id.ToString()
+                                        }).ToList();
+            ViewBag.drop2 = per; //Sayfalar arası veri taşımak için
+
+            //Müşteriler
+            List<SelectListItem> must = (from x in db.tblmusteri.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad +" "+ x.soyad,
+                                            Value = x.id.ToString()  
+                                        }).ToList();
+            ViewBag.drop3= must; //Sayfalar arası veri taşımak için
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(tblsatislar p)
+        {
+            var urun= db.tblurunler.Where(x => x.id == p.tblurunler.id).FirstOrDefault();
+            var musteri= db.tblmusteri.Where(x => x.id == p.tblmusteri.id).FirstOrDefault();
+            var personel= db.tblpersonel.Where(x => x.id == p.tblpersonel.id).FirstOrDefault();
+            p.tblurunler = urun;
+            p.tblmusteri = musteri;
+            p.tblpersonel = personel;
+            p.tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            tblsatislar tblsatislar = db.tblsatislar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BonusMvcStok/BonusMvcStok/Controllers/UrunController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusMvcStok.Models.Entity;
+
+namespace BonusMvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index(string p)
+        {
+            var urunler = from x in db.tblurunler select x;
+            if (!string.IsNullOrEmpty(p))
+            {
+                urunler = urunler.Where(x => x.ad.Contains(p));
+            }
+            return View(urunler.ToList());
+        }
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> ktg = (from x in db.tblkategori.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad,
+                                            Value = x.id.ToString()
+
+
+                                        }).ToList();
+            ViewBag.drop = ktg; //Sayfalar arası veri taşımak için
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(tblurunler t)
+        {
+            var ktgr = db.tblkategori.Where(x => x.id == t.tblkategori.id).FirstOrDefault();
+            t.tblkategori = ktgr;
+            db.tblurunler.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> kat = (from x in db.tblkategori.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.ad,
+                                             Value = x.id.ToString()
+                                         }).ToList();
+            var ktgr = db.tblurunler.Find(id);
+            ViewBag.urunkategori = kat;
+            return View("UrunGetir", ktgr);
+
+        }
+        public ActionResult UrunGuncelle(tblurunler p)
+        {
+            var urun = db.tblurunler.Find(p.id);
+            urun.marka = p.marka;
+            urun.satisfiyat = p.satisfiyat;
+            urun.stok = p.stok;
+            urun.alisfiyat = p.alisfiyat;
+            urun.ad = p.ad; 
+            var ktg = db.tblkategori.Where(x=>x.id==p.tblkategori.id).FirstOrDefault();
+            urun.kategori = ktg.id;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+        public ActionResult UrunSil (tblurunler p1)
+        {
+            var urunbul = db.tblurunler.Find(p1.id);
+            urunbul.durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
